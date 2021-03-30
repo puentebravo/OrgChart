d5e6838dc59467bb40569f112a1a0078b02d8589
Puentebravo <puentematos@gmail.com>
2021-03-30 00:00:05 -0400

    Renamed file, added static header component

---
+++ orgchart/src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function StaticHeader() {
+  return (
+    <div className="row justify-content-center">
+      <div className="col-lg-12">
+        <h1 className="display-1">Welcome to OrgChart.</h1>
+      </div>
+    </div>
+  );
+}
+
+export default StaticHeader;
