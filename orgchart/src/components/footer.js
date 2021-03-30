e5b5b7a2815652a01bc3b69601c740c32448a372
Puentebravo <puentematos@gmail.com>
2021-03-30 00:54:36 -0400

    Wrote sticky footer component

---
+++ orgchart/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/footer.css";
+
+function StickyFooter() {
+  return (
+    <footer className="footer bg-dark">
+      <div className="container">
+        <span className="text-left">(©) 2021 OrgChart</span>
+      </div>
+    </footer>
+  );
+}
+export default StickyFooter;
