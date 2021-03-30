e7018e51820f5a75408e9f6117572e37dd9c42c6
Puentebravo <puentematos@gmail.com>
2021-03-30 00:55:57 -0400

    Added basic page structure, passed in header, footer components

--- orgchart/src/pages/directory.js
+++ orgchart/src/pages/directory.js
@@ -1,3 +1,14 @@
 import React from "react";
 import API from "../utils/API";
 import StaticHeader from "../components/header";
+import StickyFooter from "../components/footer";
+
+function Directory() {
+  return (
+  <div>
+  <StaticHeader />
+  <StickyFooter />
+  </div>)
+}
+
+export default Directory;
