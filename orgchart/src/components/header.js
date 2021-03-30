f74eb6a656ecda2dccb1a22e0d9435c54697eb5f
Puentebravo <puentematos@gmail.com>
2021-03-30 00:55:30 -0400

    Added static header, fixed styling to match footer

--- orgchart/src/components/header.js
+++ orgchart/src/components/header.js
@@ -3,8 +3,8 @@ import React from "react";
 function StaticHeader() {
   return (
     <div className="row justify-content-center">
-      <div className="col-lg-12">
-        <h1 className="display-1">Welcome to OrgChart.</h1>
+      <div className="col-lg-12 bg-dark text-white">
+        <h1 className="display-4">Welcome to OrgChart.</h1>
       </div>
     </div>
   );
