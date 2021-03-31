3cca951c8282868e87e77d18b191a16c6fbde740
Puentebravo <puentematos@gmail.com>
2021-03-31 14:04:53 -0400

    Moved state monitoring into pages level

--- src/pages/directory.js
+++ src/pages/directory.js
@@ -1,14 +1,41 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import API from "../utils/API";
 import StaticHeader from "../components/header";
 import EmployeeList from "../components/employeelist";
 import StickyFooter from "../components/footer";
 
 function Directory() {
+  const [tableEntry, setTableEntry] = useState({
+    image: "",
+    firstName: "",
+    lastName: "",
+    number: [],
+    streetName: "",
+    address: "",
+    email: "",
+    phone: "",
+  });
+
+  useEffect(() => {
+    API.ping().then((res) => {
+      console.log(res);
+      setTableEntry({
+        image: res.data.results[0].picture.thumbnail,
+        firstName: res.data.results[0].name.first,
+        lastName: res.data.results[0].name.last,
+        number: res.data.results[0].location.street.number,
+        streetName: res.data.results[0].location.street.name,
+        address: res.data.results[0].location.city,
+        email: res.data.results[0].email,
+        phone: res.data.results[0].phone,
+      });
+    });
+  }, []);
+
   return (
     <div>
       <StaticHeader />
-      <EmployeeList />
+      <EmployeeList results={tableEntry}/>
       <StickyFooter />
     </div>
   );
