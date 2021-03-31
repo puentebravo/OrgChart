495692a3176414769fe3b9702c57787dea20336b
Puentebravo <puentematos@gmail.com>
2021-03-31 02:38:18 -0400

    Moved state functions to employeelist component for now

--- src/pages/directory.js
+++ src/pages/directory.js
@@ -1,13 +1,17 @@
 import React from "react";
+
 import StaticHeader from "../components/header";
+import EmployeeList from "../components/employeelist";
 import StickyFooter from "../components/footer";
 
 function Directory() {
   return (
-  <div>
-  <StaticHeader />
-  <StickyFooter />
-  </div>)
+    <div>
+      <StaticHeader />
+      <EmployeeList />
+      <StickyFooter />
+    </div>
+  );
 }
 
 export default Directory;
