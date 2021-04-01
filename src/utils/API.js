38cf15c36de50c815d465f7423930211780c0216
Puentebravo <puentematos@gmail.com>
2021-03-31 20:41:59 -0400

    Added parameters to API call to be less resource intensive

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
-const ENDPOINT = "https://randomuser.me/api/?results=50";
+const ENDPOINT = "https://randomuser.me/api/?results=50&noinfo&exc=login,registered,dob&format=prettyJSON";
 
 export default {
     ping: function() {
