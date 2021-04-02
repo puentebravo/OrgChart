0df04594fb8ac49932d2fb3d2f5e0b72e4b9ef93
Puentebravo <puentematos@gmail.com>
2021-04-02 19:11:08 -0400

    Passed state props to search form

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,14 @@
 import React from "react";
+import SearchBar from "./searchform";
 
-function StaticHeader() {
+function StaticHeader(props) {
   return (
     <nav className="navbar navbar-light bg-dark">
-  <div className="container-fluid">
-    <h4 className="display-4 text-white">Welcome to OrgChart</h4>
-    <form className="d-flex">
-      <input className="form-control me-2" type="search" placeholder="Search Directory" aria-label="Search"/>
-      <button className="btn btn-outline-warning" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+      <div className="container-fluid">
+        <h4 className="display-4 text-white">Welcome to OrgChart</h4>
+        <SearchBar search={props.filter} handleInputChange={props.handleInputChange} handleFormSubmit={props.handleFormSubmit} />
+      </div>
+    </nav>
   );
 }
 
